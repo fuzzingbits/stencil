c77a2c4fb5cf75a87acfe266e7dc93b7d83b3e14
Aaron Ellington <aaron@ellington.io>
2021-03-14 09:50:20 -0400

    wip

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+
+class DefaultController extends AbstractController
+{
+    public function indexAction(): Response
+    {
+        return new Response('Index.');
+    }
+
+    public function helloAction(): Response
+    {
+        return new Response('Hello there.');
+    }
+}
